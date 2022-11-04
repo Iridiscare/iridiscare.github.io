2726afe2fbbd2153fe505fc222da5e257b903512
David Martin-Corral <david@zenseiapp.com>
2022-11-04 13:15:58 +0100

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,14 +4,13 @@ var menuContainer = document.querySelector('#main-menu-mobile');
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
+    menuTrigger.classList.toggle('is-active');
+    body.classList.toggle('lock-scroll');
 }
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
