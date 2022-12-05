0b7018ea7d9d174fce713f2243d4fd7f460ae5aa
David Martin-Corral <david@zenseiapp.com>
2022-12-05 11:57:11 +0100

    behavioural channges

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,11 +3,18 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+var menuSections = document.querySelector('#main-menu-mobile > ul');
+
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active');
-    body.classList.toggle('lock-scroll');
+    // body.classList.toggle('lock-scroll');
+}
+
+menuSections.onclick = function() {
+  menuContainer.classList.remove('open');
+  menuTrigger.classList.remove('is-active');
 }
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -17,47 +24,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
-});
-
-// typing effect
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
-
-const textArray = ["reducir tu estrés.", "relajarte.", "disfrutar del ahora.", "no volverte tucu tucu."];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
