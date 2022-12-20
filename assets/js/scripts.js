379756ab452d51451c94827f2b9a9ef984f65a46
David Martin-Corral <david@zenseiapp.com>
2022-12-20 11:49:23 +0100

    sticky pop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,3 +25,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+let hasBannerBeenClosed = false;
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  
+  if (y > 6500 && !hasBannerBeenClosed) {
+    $(".top-banner").slideDown();
+  } else {
+    $(".top-banner").slideUp();
+  }
+});
+
+$("#closeTopBanner").on("click",function(){
+  $(".top-banner").slideUp();
+  hasBannerBeenClosed = true;
+});
