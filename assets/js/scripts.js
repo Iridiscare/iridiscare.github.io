3c4e2742b8ba2944b168199fe4e209a04fc0c8d3
David Martin-Corral <david@zenseiapp.com>
2022-12-21 18:09:38 +0100

    faq!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -41,3 +41,18 @@ $("#closeTopBanner").on("click",function(){
   $(".top-banner").slideUp();
   hasBannerBeenClosed = true;
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
